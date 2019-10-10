b6dd527a722a6579ca6c56b751c66deee3e4b4ea
Karen753 <sulca753@gmail.com>
2019-10-10 08:57:58 -0500

    js-css-html listo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
-document.getElementById("pag2").classList.add("ocultar");
-document.getElementById("pag3").classList.add("ocultar");
+
+
+document.getElementById('pag2').style.display = 'none'
+document.getElementById('pag3').style.display = 'none'
+document.getElementById('pag4').style.display = 'none'
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+    document.getElementById('pag2').style.display = 'block'
+    document.getElementById('pag1').style.display = 'none'
+    document.getElementById('pag3').style.display = 'none'
+    document.getElementById('pag4').style.display = 'none'
+
+});
+
+const cifrar = document.getElementById('cifrar');
+
+cifrar.addEventListener('click', () => {
+    document.getElementById('pag3').style.display = 'block'
+    document.getElementById('pag1').style.display = 'none'
+    document.getElementById('pag2').style.display = 'none'
+    document.getElementById('pag4').style.display = 'none'
+    
+});
+
+
+const volver = document.getElementById('volver');
+
+volver.addEventListener('click', () => {
+    document.getElementById('pag1').style.display = 'block'
+    document.getElementById('pag2').style.display = 'none'
+    document.getElementById('pag3').style.display = 'none'
+    document.getElementById('pag4').style.display = 'none'
+});
+
+const descifrar = document.getElementById('descifrar');
+
+    descifrar.addEventListener('click', () => {
+    document.getElementById('pag4').style.display = 'block'
+    document.getElementById('pag1').style.display = 'none'
+    document.getElementById('pag2').style.display = 'none'
+    document.getElementById('pag3').style.display = 'none'
+
+    });
+    
+    const volver2 = document.getElementById('volver2');
+
+volver2.addEventListener('click', () => {
+    document.getElementById('pag1').style.display = 'block'
+    document.getElementById('pag2').style.display = 'none'
+    document.getElementById('pag3').style.display = 'none'
+    document.getElementById('pag4').style.display = 'none'
+});
+
+    
+   
+    
+
+
+
+
+
+
+
+
+
 
